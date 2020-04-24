4aa36c9e5be77a0a085b612a31d7998c9a96e25f
Oleg Primachenko <primachenko_oleg@outlook.com>
2020-04-24 16:10:55 +0100

    dac8532 registers and usage examples added

--- src/DAC8532.c
+++ src/DAC8532.c
@@ -1,34 +1,24 @@
 #include "DAC8532.h"
 #include "debug.h"
 
-static void DAC8532_Write(uint8_t channel, uint16_t reg_val)
+void DAC8532_Write(uint8_t reg_cmd, uint16_t reg_data)
 {
     DEV_Digital_Write(DEV_CS1_PIN, 1);
     DEV_Digital_Write(DEV_CS1_PIN, 0);
-    DEV_SPI_WriteByte(channel);
-    DEV_SPI_WriteByte((reg_val >> 8));
-    DEV_SPI_WriteByte((reg_val & 0xff));
+    DEV_SPI_WriteByte(reg_cmd);
+    DEV_SPI_WriteByte((reg_data >> 8));
+    DEV_SPI_WriteByte((reg_data & 0xff));
     DEV_Digital_Write(DEV_CS1_PIN, 1);
 }
 
 void DAC8532_Set_Voltage(uint8_t channel, double voltage)
 {
-    if (DAC8532_CHANNEL_A != channel &&
-        DAC8532_CHANNEL_B != channel)
-    {
-        DAC8532_ERR("invalid channel");
-        return;
-    }
-
     if (DAC8532_VREF < voltage)
     {
         DAC8532_ERR("invalid voltage value");
         return;
     }
 
-    uint16_t reg_val = (uint16_t) (voltage * DAC8532_REG_VALUE_MAX / DAC8532_VREF);
-    DAC8532_Write(channel, reg_val);
-    DAC8532_DBG("out voltage %f for channel %s was setted",
-                 voltage, DAC8532_CHANNEL_A == channel ?
-                 DAC8532_CHANNEL_A_STR:DAC8532_CHANNEL_B_STR);
+    uint16_t reg_data = (uint16_t) (voltage * DAC8532_REG_VALUE_MAX / DAC8532_VREF);
+    DAC8532_Write(channel, reg_data);
 }
